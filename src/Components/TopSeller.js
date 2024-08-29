f984ba61b4acb3f2c239feea80786142e451f5d9
kimcg89 <kimcg891@gmail.com>
2024-08-29 16:36:29 +0900

    first deploy

---
+++ src/Components/TopSeller.js
@@ -0,0 +1,15 @@
+import Seller1 from '../img/seller1.jpg'
+import Seller2 from '../img/seller2.jpg'
+import Seller3 from '../img/seller3.jpg'
+import Seller4 from '../img/seller4.jpg'
+import Seller5 from '../img/seller5.jpg'
+
+const TopSeller = [
+  { id : 1, seller_name : 'Jane Copper', username : '@jane', imgSrc : Seller1},
+  { id : 2, seller_name : 'Ariene Macooy', username : '@ariene', imgSrc : Seller2},
+  { id : 3, seller_name : 'Theresa Webb', username : '@theresa', imgSrc : Seller3},
+  { id : 4, seller_name : 'Raplh edwards', username : '@ralph', imgSrc : Seller4},
+  { id : 5, seller_name : 'james bond', username : '@james', imgSrc : Seller5},
+]
+
+export default TopSeller
